2b468eb978acda39fa746aa573183c461dec3c1c
Pranay-Tej <s.pranayteja@gmail.com>
2023-10-04 22:03:28 +0530

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LineChart from "./LineChart";
 export default function App() {
   return (
     <>
-      <BarChart />;
+      <BarChart />
       <LineChart />
     </>
   );
--- src/LineChart.tsx
+++ src/LineChart.tsx
@@ -3,6 +3,7 @@ import { Group } from "@visx/group";
 import { scaleLinear, scaleTime } from "@visx/scale";
 import { LinePath } from "@visx/shape";
 import { curveMonotoneX } from "@visx/curve";
+import { Grid } from "@visx/grid";
 
 const data = {
   "2023-09-01": 50,
@@ -25,7 +26,7 @@ const height = 300;
 const width = 500;
 const margin = {
   top: 20,
-  bottom: 20,
+  bottom: 40,
   left: 60,
   right: 20,
 };
@@ -46,6 +47,18 @@ export default function LineChart() {
       <h2>Line Chart</h2>
       <svg width={width} height={height}>
         <Group>
+          <Grid
+            xScale={xScale}
+            yScale={yScale}
+            width={width - margin.right}
+            height={height - margin.bottom}
+            // numTicksRows={0} // This ensures no horizontal grid lines are drawn.
+            stroke="#E9ECF0" // This gives a light-colored line. Adjust the color as needed.
+            strokeWidth={1}
+            // strokeDasharray="4,2" // Optional: makes the line dashed.
+            columnTickValues={transformedData.map((d) => d.date)} // Provide tickValues to Grid
+            rowTickValues={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+          />
           <LinePath
             data={transformedData}
             x={(d) => xScale(d.date)}
@@ -55,7 +68,11 @@ export default function LineChart() {
             fill="none"
             curve={curveMonotoneX}
           />
-          <AxisLeft scale={yScale} left={margin.left} />
+          <AxisLeft
+            scale={yScale}
+            left={margin.left}
+            tickValues={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+          />
           <AxisBottom
             scale={xScale}
             top={height - margin.bottom}
